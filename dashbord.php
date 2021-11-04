07cc6a452912072e6b6300af9d093741b5a491a2
zio <saad_zizi@hotmail.com>
2021-11-04 23:57:32 +0100

    add session start to dashboard

--- dashbord.php
+++ dashbord.php
@@ -1,3 +1,7 @@
 <?php
-
+  session_start();
+if (!(isset($_SESSION['loggedin'])) or $_SESSION['loggedin'] == null) {
+    header('Location: index.php');
+    exit;
+}
 echo "welcome to dashbord page";
